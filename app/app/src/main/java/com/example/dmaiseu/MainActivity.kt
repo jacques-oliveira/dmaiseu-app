c52be7afe84d3b993529045519eb04228e15320e
jacques-oliveira <jacques.oliveira.eng@gmail.com>
2024-01-14 18:30:25 -0300

    FragmentInfo

--- app/app/src/main/java/com/example/dmaiseu/MainActivity.kt
+++ app/app/src/main/java/com/example/dmaiseu/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.dmaiseu
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProvider
 import com.example.dmaiseu.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
@@ -40,8 +44,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 .replace(R.id.fragment_container, ProfileFragment()).commit()
             navigationView.setCheckedItem(R.id.nav_perfil)
         }
-
-
+        val sharedPrefs: SharedPreferences = this.getSharedPreferences("SHARED_PREFS_USER", Context.MODE_PRIVATE)
+        val user_name_drawer : TextView = findViewById<NavigationView?>(R.id.nav_view).getHeaderView(0).findViewById(R.id.user_name_drawer)
+        user_name_drawer.text = sharedPrefs.getString("user_name",null)!!.split(" ")[0]
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
